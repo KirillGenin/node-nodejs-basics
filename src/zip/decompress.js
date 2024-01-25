fec9f1d09709f22c81248f67ee892e2de71d18d3
KirillGenin <332211op@gmail.com>
2024-01-25 14:23:32 +0300

    refactor: remove archiv after decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -3,14 +3,22 @@ import { createReadStream, createWriteStream } from 'node:fs';
 import { createUnzip } from 'node:zlib';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import fs from 'fs/promises';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToFileToCompress = path.resolve(
+  __dirname,
+  'files',
+  'fileToCompress.txt'
+);
+const pathToArchive = path.resolve(__dirname, 'files', 'archive.gz');
 
 const decompress = async () => {
-    const readStream = createReadStream(path.resolve(__dirname, 'files', 'archive.gz'));
-    const writeStream = createWriteStream(path.resolve(__dirname, 'files', 'fileToCompress.txt'));
+  const readStream = createReadStream(pathToArchive);
+  const writeStream = createWriteStream(pathToFileToCompress);
 
-    await pipeline(readStream, createUnzip(), writeStream);
+  await pipeline(readStream, createUnzip(), writeStream);
+  fs.rm(pathToArchive);
 };
 
 await decompress();
