b82518f595eb2a46f646dc324abee79b2faf2eb6
KirillGenin <332211op@gmail.com>
2024-01-25 14:20:09 +0300

    refactor: remove file after compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -3,14 +3,22 @@ import { createReadStream, createWriteStream } from 'node:fs';
 import { createGzip } from 'node:zlib';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import fs from 'fs/promises';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToFileToCompress = path.resolve(
+  __dirname,
+  'files',
+  'fileToCompress.txt'
+);
+const pathToArchive = path.resolve(__dirname, 'files', 'archive.gz');
 
 const compress = async () => {
-    const readStream = createReadStream(path.resolve(__dirname, 'files', 'fileToCompress.txt'));
-    const writeStream = createWriteStream(path.resolve(__dirname, 'files', 'archive.gz'));
+  const readStream = createReadStream(pathToFileToCompress);
+  const writeStream = createWriteStream(pathToArchive);
 
-    await pipeline(readStream, createGzip(), writeStream);
+  await pipeline(readStream, createGzip(), writeStream);
+  fs.rm(pathToFileToCompress);
 };
 
 await compress();
